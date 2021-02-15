0c48d6dbb7ab2d35a435ca6490b4d82c6a374eee
yuji moriya <voyager.nasa0928@gmail.com>
2021-02-16 00:40:28 +0900

    bootstrap関連の修復完了

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
-//= require jquery
 //= require_tree .
