7705ed811da58d6165fda0c76c5665f06aa0b620
yuji moriya <voyager.nasa0928@gmail.com>
2021-02-16 21:23:23 +0900

    herokuアップロード用設定

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require rails-ujs
 //= require turbolinks
 //= require jquery/dist/jquery.js
+//= require bootstrap
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  config.assets.precompile +=  %w( *.woff *.eot *.svg *.ttf )
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
